6287601dfeaf6a10c37ce6c23dbdb04cb7f59aee
Idinaldo Teteo <idinaldoteteo@gmail.com>
2025-03-07 18:56:52 +0000

    add script to add itens on tables

---
+++ Order/src/main/resources/db/migration/V3__insert_tabela_pedidos.sql
@@ -0,0 +1,7 @@
+insert into alurafood_order.orders (datetime, status)
+values
+    ('2024-12-28 13:26:12', 'REALIZED'),
+    ('2025-01-02 13:26:12', 'REALIZED'),
+    ('2025-01-22 13:26:12', 'REALIZED'),
+    ('2025-02-02 13:26:12', 'REALIZED'),
+    ('2025-03-02 13:26:12', 'REALIZED');
---
+++ Order/src/main/resources/db/migration/V4__insert_tabela_item_pedido.sql
@@ -0,0 +1,12 @@
+insert into alurafood_order.order_item (description, quantity, order_id)
+values
+    ('Coca cola', 5, 1),
+    ('Sandwiche', 2, 1),
+    ('Food', 1, 2),
+    ('alad', 2, 2),
+    ('Cake', 2, 3),
+    ('bread', 2, 3),
+    ('Cheese', 5, 4),
+    ('Hamburguer', 5, 4),
+    ('gg', 5, 5),
+    ('Sprit', 5, 5);
